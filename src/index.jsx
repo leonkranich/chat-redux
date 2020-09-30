c408dd5292ab22d7c6ef16c2a03fb07312f21793
leon kranich <leon_kranich@yahoo.de>
2020-09-30 12:24:51 +0200

    slack clone finished:)

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,50 @@
-// external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
+import logger from 'redux-logger';
 import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+// State and reducers
 import messagesReducer from './reducers/messages_reducer';
 import selectedChannelReducer from './reducers/selected_channel_reducer';
 
-const initialState = {
-  messages: [],
-  channels: ['general', 'cool react stuff', 'berlin my love'],
-  selectedChannel: 'general',
-  currentUser: prompt("Hey awesome, what's your name?")
-};
-
-// State and reducers
 const identityReducer = (state = null) => state;
+
+const initialState = {
+  messages: [{
+    "author":"anonymous92",
+    "content":"Hello world!",
+    "created_at":"2017-09-26T23:03:16.365Z"
+  },
+  {
+    "author":"anonymous77",
+    "content":"My name is anonymous77",
+    "created_at":"2017-09-26T23:03:21.194Z"
+  }],
+  channels: ['general', 'react', 'paris'],
+  currentUser: prompt("What is your username?"),
+  selectedChannel: 'general'
+};
 
 const reducers = combineReducers({
   messages: messagesReducer,
-  selectedChannel: selectedChannelReducer,
   channels: identityReducer,
-  currentUser: identityReducer
+  currentUser: identityReducer,
+  selectedChannel: selectedChannelReducer
 });
 
-const middlewares = applyMiddleware(reduxPromise, logger); 
+// Middlewares
+const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
